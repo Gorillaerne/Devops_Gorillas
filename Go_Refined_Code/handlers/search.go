8f6c88d54f48b2fd4eafb514d94eeaaa7ba1389f
malthe-kea <mati0002@stud.kea.dk>
2026-02-10 12:01:47 +0100

    Opdateret layout.html til Gorilla.

--- Go_Refined_Code/handlers/search.go
+++ Go_Refined_Code/handlers/search.go
@@ -1,37 +1,46 @@
-package
+package handlers
 
-import "net/http"
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query().Get("q")
+import (
+	"devops_gorillas/database"
+	"html/template"
+	"net/http"
+)
 
-	var results []SearchResult
+type SearchResult struct {
+	Title       string
+	Description string
+	URL         string
+}
 
-	if q != "" {
-		rows, err := database.DB.Query(`
-			SELECT title, description, url
-			FROM pages
-			WHERE title LIKE ? OR description LIKE ?
-			LIMIT 20
-		`, "%"+q+"%", "%"+q+"%")
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
+func SearchHandler(tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query().Get("q")
+
+		var results []SearchResult
+
+		if q != "" {
+			rows, err := database.DB.Query(`
+				SELECT title, description, url
+				FROM pages
+				WHERE title LIKE ? OR description LIKE ?
+				LIMIT 20
+			`, "%"+q+"%", "%"+q+"%")
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			defer rows.Close()
+
+			for rows.Next() {
+				var r SearchResult
+				rows.Scan(&r.Title, &r.Description, &r.URL)
+				results = append(results, r)
+			}
 		}
-		defer rows.Close()
 
-		for rows.Next() {
-			var r SearchResult
-			rows.Scan(&r.Title, &r.Description, &r.URL)
-			results = append(results, r)
-		}
+		tmpl.ExecuteTemplate(w, "search.html", map[string]any{
+			"search_results": results,
+			"q":              q,
+		})
 	}
-
-	tmpl.ExecuteTemplate(w, "search.html", map[string]any{
-		"search_results": results,
-		"request": map[string]any{
-			"args": map[string]string{
-				"q": q,
-			},
-		},
-	})
 }
