8e245ccd2d4653951b81dd1df8ab19d98f80b214
gustavtjac <gustavtorup@gmail.com>
2026-03-23 11:46:12 +0100

    Fixed scripts to also add new stylings

--- Go_Refined_Code/static/javaScript/search_script.js
+++ Go_Refined_Code/static/javaScript/search_script.js
@@ -24,22 +24,31 @@ searchButton.addEventListener("click", () => {
 function renderSearchResults(searchResults) {
 
     resultsDiv.innerHTML = ""
-    console.log(searchResults)
+
     searchResults.forEach(searchResult => {
-        console.log(searchResult)
-        const searchResultDiv = document.createElement("div")
-        const searchResultH2 = document.createElement("H2")
-        const searchResultA = document.createElement("a")
-        searchResultA.textContent = searchResult.title
-        searchResultA.classList.add("search-result-title")
-        searchResultA.href = searchResult.URL
-        searchResultH2.appendChild(searchResultA)
+        const item = document.createElement("div")
+        item.classList.add("result-item")
 
-        searchResultDiv.appendChild(searchResultH2)
+        const link = document.createElement("a")
+        link.href = searchResult.URL
 
-        resultsDiv.appendChild(searchResultDiv)
+        const urlSpan = document.createElement("span")
+        urlSpan.classList.add("result-url")
+        urlSpan.textContent = searchResult.URL
 
+        const titleSpan = document.createElement("span")
+        titleSpan.classList.add("result-title")
+        titleSpan.textContent = searchResult.title
 
+        const snippetSpan = document.createElement("span")
+        snippetSpan.classList.add("result-snippet")
+        snippetSpan.textContent = searchResult.content
+
+        link.appendChild(urlSpan)
+        link.appendChild(titleSpan)
+        link.appendChild(snippetSpan)
+        item.appendChild(link)
+        resultsDiv.appendChild(item)
     })
 
 }
